10d8ae1b7cbdb931df5b5ee6144a90bd880efdc7
Lionsinthesky <109576093+Lionsinthesky@users.noreply.github.com>
2026-04-14 11:36:02 +0800

    experimenting

--- dataset_preprocessing.py
+++ dataset_preprocessing.py
@@ -26,7 +26,7 @@ def create_dataset_csv(dataset_path):
 def audio_to_mel_spectrogram(file_path, n_mels=128, fmax=8000, sr = 44100):
     y, sr = librosa.load(file_path, sr=sr)  # Load audio file
     #S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, fmax=fmax)  # Compute Mel spectrogram
-    S = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mels)  # Compute MFCCs from the Mel spectrogram
+    S = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mels, fmax=fmax)  # Compute MFCCs from the Mel spectrogram
     S_dB = librosa.power_to_db(S, ref=np.max)  # Convert to decibel scale
     
     return S_dB
@@ -39,10 +39,9 @@ def convert_audio_to_mel_spectrogram(dataset_path):
     
     return data_mel_spectrogram
 
-def normalize_split_data(x_train, y_train, x_test, y_test, val_size=0.2):
+def normalize_split_data(x_train, y_train, val_size=0.2):
     #normalize the data
-    x_train = np.array(x_train)/255.0
-    x_test = np.array(x_test)/255.0
+    x_train = (x_train - np.mean(x_train, axis=0)) / np.std(x_train, axis=0)
 
     # Split data
     val_size_int = int(np.round(len(x_train))*val_size)
@@ -51,4 +50,4 @@ def normalize_split_data(x_train, y_train, x_test, y_test, val_size=0.2):
     x_train = x_train[:-val_size_int]
     y_train = y_train[:-val_size_int]
 
-    return x_train, y_train, x_val, y_val, x_test, y_test
+    return x_train, y_train, x_val, y_val
--- run_cnn.py
+++ run_cnn.py
@@ -4,13 +4,25 @@ from tensorflow import keras
 def convolutional_model(input_shape):
     # Define a convolutional model with two convolutional layers and two max pooling layers
     inputs = keras.Input(shape=input_shape)
-    x = keras.layers.Conv2D(16, kernel_size=(3), activation='relu')(inputs)
+    x = keras.layers.Conv2D(16, kernel_size=(3, 3), activation='relu')(inputs)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.MaxPooling2D(pool_size=2)(x)
-    x = keras.layers.Conv2D(32, kernel_size=3, activation='relu')(x)
+
+    x = keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu')(x)
+    x = keras.layers.BatchNormalization()(x)
     x = keras.layers.MaxPooling2D(pool_size=2)(x)
-    x = keras.layers.Dropout(0.25)(x)
+
+    #reshape to RNN input
+    #shape = keras.backend.int_shape(x)
+    #x = keras.layers.Reshape(target_shape=(shape[1], shape[2] * shape[3]))(x)
+
+    #x = keras.layers.Bidirectional(keras.layers.LSTM(32, return_sequences=True))(x)
+    #x = keras.layers.Bidirectional(keras.layers.LSTM(16))(x)
+
+    x = keras.layers.Dropout(0.5)(x)
     x = keras.layers.Flatten()(x)
-    x = keras.layers.Dense(10, activation='softmax')(x)
+    #x = keras.layers.GlobalAveragePooling2D()(x)
+    x = keras.layers.Dense(5, activation='softmax')(x)
 
     conv_model = keras.Model(inputs=inputs, outputs=x)
   
